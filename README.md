ff66435ce0b0e26a8a3628095856ab918c404dc1
Harmeet10000 <harmeetsinghfbd@gmail.com>
2025-06-07 17:55:57 +0530

    chore: First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# File Upload API Example in Golang
+
+In this example we will walk-through a Simple to Complex implementation of Golang File Upload.
+
+## Features
+
+- [X] File Upload With Size limit
+- [ ] Multiple File Upload
+- [ ] Streaming / Back-pressure File Upload - Large Files
+- [ ] Retry & Circuit Breaker mechanism
+- [ ] Rate Limiting and Mime Type filter
+- [ ] Background File Processing - Generate a low-res thumbnail
+- [ ] Multiple storage example - S3, Local Storage
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/Harmeet10000/go-file-upload-api/localstorage"
+	"github.com/Harmeet10000/go-file-upload-api/s3storage"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Ensure upload directory exists
+	if err := os.MkdirAll(localstorage.UploadDir, os.ModePerm); err != nil {
+		log.Fatalf("failed to create upload dir: %v", err)
+	}
+	router := gin.Default()
+
+	// NOOB Mistake 1: Not setting the max upload size
+	// Max upload size: 10MB
+	router.MaxMultipartMemory = 10 << 20 // 10 MiB
+
+	router.POST("/upload", localstorage.HandleUpload)
+	router.GET("/uploads/:filename", localstorage.HandleGetFile)
+	router.POST("/s3/upload", s3storage.HandleUpload)
+
+	log.Println("Server running at http://localhost:8080")
+	router.Run(":8080")
+}
